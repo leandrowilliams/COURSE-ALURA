c83cd34efcea1466a515709ae3b0c234427a5cb4
leandrowilliams <leandrowilliams@outlook.com>
2022-03-10 00:59:25 -0300

    COURSE-ALURA

---
+++ REFATORACAO/intellij-idea-aula-2/intellij-idea-aula-2/src/br/com/alura/bytebank/registrador/RegistroDePagamento.java
@@ -0,0 +1,54 @@
+package br.com.alura.bytebank.registrador;
+
+import br.com.alura.bytebank.model.Pagamento;
+import br.com.alura.bytebank.model.Tipo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RegistroDePagamento {
+
+    private final static List<Pagamento> PAGAMENTOS = new ArrayList<>();
+
+    public void registra(List<Pagamento> pagamentos) {
+        for (int i = 0; i < pagamentos.size(); i++) {
+            Pagamento pagamento = pagamentos.get(i);
+            verificaTipo(pagamento);
+        salva(pagamento);
+        }
+        exibeEfetuados();
+    }
+
+    private void exibeEfetuados() {
+        if (!PAGAMENTOS.isEmpty()) {
+            System.out.println("Todos os pagamentos");
+            for (int i = 0; i < PAGAMENTOS.size(); i++) {
+                System.out.println(PAGAMENTOS.get(i));
+            }
+        } else {
+            System.out.println("Não temos pagamentos");
+        }
+    }
+
+    private void verificaTipo(Pagamento pagamento) {
+        extracted(pagamento);
+        if (pagamento.getTipo().equals(Tipo.DINHEIRO)) {
+            salva(pagamento);
+        }
+    }
+
+    private void extracted(Pagamento pagamento) {
+        if (pagamento.getTipo().equals(Tipo.CREDITO)) {
+            pagamento.setValor(pagamento.getValor() * 1.02);
+        }
+        if (pagamento.getTipo().equals(Tipo.DEBITO)) {
+            pagamento.setValor(pagamento.getValor() * 1.001);
+        }
+    }
+
+    private void salva(Pagamento pagamento) {
+        PAGAMENTOS.add(pagamento);
+        System.out.println("Pagamento realizado " + pagamento);
+    }
+
+}
